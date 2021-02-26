0b9dce2c153a2e7eaf9cb152222ffe14d34b1b99
mugambi645 <mugambi645@gmail.com>
2021-02-26 14:53:32 +0300

    application skeleton

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+users = [];
+io.on("connection", function (socket) {
+  console.log("A user connected");
+  socket.on("setUsername", function (data) {
+    if (users.indexOf(data) > -1) {
+      users.push(data);
+      socket.emit("userSet", { username: data });
+    } else {
+      socket.emit(
+        "userExists",
+        data + " username is taken! Try some other username."
+      );
+    }
+  });
+});
+
+http.listen(3000, function () {
+  console.log("listening on localhost:3000");
+});
